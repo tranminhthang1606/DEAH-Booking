24d53509d01cff2aff49a9623a2860871b1d065e
quoc-phong9990 <139205778+quoc-phong9990@users.noreply.github.com>
2024-06-08 13:42:23 +0700

    gdien home

--- DEAH/src/main.tsx
+++ DEAH/src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css";
+import { QueryClient, QueryClientProvider, useQueryClient } from '@tanstack/react-query'
 
+
+const queryclient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
+<QueryClientProvider client={queryclient}>
+
+<BrowserRouter>
+
+<App/>
+
+</BrowserRouter>
+
+</QueryClientProvider>
 )
