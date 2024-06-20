25d5576b8ea6b6d161bf7dde5026d10003d32687
Thắng Trần Minh <84889893+tranminhthang1606@users.noreply.github.com>
2024-06-20 16:25:20 +0700

    Revert "admin"

--- resources/views/layout/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-    </div>
-    <!-- Default to the left -->
-    <strong>VŨ VŨ TRỤ <a href="https://adminlte.io"></a>.</strong> 
-  </footer>
