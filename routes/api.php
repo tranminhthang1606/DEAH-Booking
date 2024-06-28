5d3e35727b1c5e9e20bb83b85d696034f1ca44f4
truog-ann <truogaan232@gmail.com>
2024-06-28 13:33:47 +0700

    user manage

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Client\TourController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\PostController;
+use App\Http\Controllers\Client\UserController;
 use App\Models\Province;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -35,6 +36,14 @@ Route::group(['middleware' => 'cors'], function () {
         //post
         Route::any('get-posts-list', [PostController::class, 'index']);
         Route::get('get-post-detail/{id}', [PostController::class, 'show']);
+        //user
+        Route::group(['prefix' => 'user'], function () {
+            Route::post('login', [UserController::class, 'login']);
+            Route::post('signup', [UserController::class, 'signup']);
+            Route::post('update/{token}', [UserController::class, 'update']);
+            Route::get('logout', [UserController::class, 'logout']);
+
+        });
     });
 
 });
