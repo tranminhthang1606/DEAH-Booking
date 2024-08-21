6d4a017767d987878ce447b883208df3a02998b4
Vu-PH29926 <vuttph29926@fpt.edu.vn>
2024-08-22 00:44:59 +0700

    sửa lại 1 vài cái

--- database/migrations/2024_06_05_020909_create_hotels_table.php
+++ database/migrations/2024_06_05_020909_create_hotels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('price');
+            $table->string('price');
             $table->string('promotion');
             $table->text('description');
             $table->foreignId('province_id')->constrained()->on('provinces');
